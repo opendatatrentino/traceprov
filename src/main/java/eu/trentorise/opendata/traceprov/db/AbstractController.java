9d757ea9e553e237906a140545304f1701af34ef
David Leoni <davidleonigit-github@yahoo.com>
2015-11-23 16:09:17 +0100

    added AbstractController

---
+++ src/main/java/eu/trentorise/opendata/traceprov/db/AbstractController.java
@@ -0,0 +1,17 @@
+package eu.trentorise.opendata.traceprov.db;
+
+import org.immutables.value.Value;
+
+import eu.trentorise.opendata.commons.BuilderStylePublic;
+
+/**
+ * Default implementation of a controller.
+ * 
+ * @author David Leoni
+ *
+ */
+@Value.Immutable
+@BuilderStylePublic
+class AbstractController extends AController {
+    
+}
